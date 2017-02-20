7f9c95069534fe713daa67ee2b279eb93a0c864e
Jeremy Shankle <jpshankle@gmail.com>
2017-02-20 17:56:07 -0500

    add auto plugin

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [scad-clj "0.5.2"]]
+  :plugins [[lein-auto "0.1.3"]]
   :main ^:skip-aot ring.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
