e5d0374c9f55ba1f2389b747562ec7f651892551
Jeremy Shankle <jpshankle@gmail.com>
2017-02-20 17:56:21 -0500

    update to map

--- src/ring/core.clj
+++ src/ring/core.clj
@@ -4,32 +4,32 @@
            [scad-clj.model :refer :all]))
 
 (def standardSize {
-  :squareone 1.5
+  :squareone 1.8
   :squaretwo 7
-  :translation [12, 0, 0]})
+  :translation [12.5, 0, 0]})
+
+(def ringShapes [
+  [[0 0 0]   [-0.15 0 0]]
+  [[0.7 0 0] [0.15 0 0]]
+  [[0 0.7 0] [0 0.15 0]]])
 
 (defn oneRing
   "One ring that goes with more rings"
-  [{:keys [squareone squaretwo translation]} outertranslation rotation]
+  [{:keys [squareone squaretwo translation]} [outertranslation rotation]]
   (translate outertranslation
   (rotatec rotation
     (extrude-rotate
       (translate translation
         (square squareone squaretwo))))))
 
-(def twodring
-  (union
-    (oneRing standardSize
-      [0 0 0]
-      [-0.15 0 0])
-    (oneRing standardSize
-      [0.7 0 0]
-      [0.15 0 0])
-    (oneRing standardSize
-      [0 0.7 0]
-      [0 0.15 0])))
+(defn twodring
+  "creates a ring from a size and a vector of rings"
+  [size rings]
+  (map #(oneRing size %) rings))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "creates a ring scad file"
   [& args]
-  (spit "dist/2dring.scad" (write-scad twodring)))
+  (spit "dist/2dring.scad"
+    (write-scad
+      (twodring standardSize ringShapes))))
